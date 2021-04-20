fcaacf1899658fc14b4e4877b8f4da3b50736e88
MandorSavall <bogdan.lyamzin.d@gmail.com>
2021-04-20 19:14:42 +0300

    add new lesson

---
+++ example-1/products.js
@@ -0,0 +1,16 @@
+const products = [
+    {
+        _id: "2tfsfdfd",
+        name: "Lenovo X120S"
+    },
+    {
+        _id: "54ffhh75",
+        name: "Samsung Note 4"
+    },
+    {
+        _id: "g648744d",
+        name: "iPhone X"
+    }
+];
+
+module.exports = products;
---
+++ example-3/products.js
@@ -0,0 +1,19 @@
+const products = [
+    {
+        _id: "2tfsfdfd",
+        name: "Lenovo X120S",
+        brand: "ThinkPad"
+    },
+    {
+        _id: "54ffhh75",
+        name: "Samsung Note 4",
+        brand: "Note"
+    },
+    {
+        _id: "g648744d",
+        name: "iPhone X",
+        brand: "iPhone"
+    }
+];
+
+module.exports = products;
